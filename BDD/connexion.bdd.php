733744c0a4366aaf56134dca8b801c23efd0a3a7
XuanWEI24 <mllexuanwei@gmail.com>
2020-07-23 17:41:52 +0800

    bdd

--- BDD/connexion.bdd.php
+++ BDD/connexion.bdd.php
@@ -4,9 +4,6 @@
         function connect(){
                try
                {  
-                    //$bdd = new PDO("mysql:host=localhost;dbname=talentland;charset=utf8","root","");
-                    
-                    //1ère méthode
                     $aConfig = parse_ini_file($_SESSION["APPLICATION"].'/config.php');                     
                     $bdd = new PDO("mysql:host=".$aConfig['HOST'].";dbname=".$aConfig['BD'].";charset=utf8",$aConfig['LOGIN'],$aConfig['PASS']);
                     
