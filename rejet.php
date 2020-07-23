78b96b4ba55e198709e4de44c023d657497b9ac4
XuanWEI24 <mllexuanwei@gmail.com>
2020-07-23 17:35:14 +0800

    rejet

--- rejet.php
+++ rejet.php
@@ -1,84 +1,16 @@
+<?php session_name('CHEMIN'); session_start(); ?>
 <!doctype html>
-<?php
-    require_once 'http://localhost:8888/XAMPP/XAMPP_exe/htdocs/QSF-reorganized-/FONCTIONCOMMUN/fonctionutile.php';
-    require_once 'http://localhost:8888/XAMPP/XAMPP_exe/htdocs/QSF-reorganized-/FONCTIONNALITE/header.php'; 
-?>
-    
-  <body>
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="Accueil.php">Quai des savoir-faire</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="Accueil.php">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="Besoin.php">Besoins</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="Talent.php">Talents</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="AbonnerCategorie.php">Catégories</a>
-          </li>  
-        </ul>
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item dropleft">   
-            <?php
-            require_once 'Fonctions.php';
-            
-            if(isset($_SESSION['email'])){
-                    echo('<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
-                    echo $_SESSION['email'];       // quand l'utiliateur n'a pas croché le case Anonyme au moment de l'inscription, on va afficher son adresse mail
-                    echo('</a>');
-            } else {
-                echo('<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">');
-                echo "Visiteur";                   //Utilisateur qui n'a pas conncté
-                echo('</a>');
-            } 
-            ?>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <?php
-                if(isset($_SESSION['email'])){
-                    echo ('<a class="dropdown-item" href="MonProfil.php">Mon profil</a>');
-                    echo ('<a class="dropdown-item" href="MesCategories.php">Mes catégories</a>');
-                    echo ('<a class="dropdown-item" href="Deconnecter.php" onclick="Deconnexion()">Déconnecter</a>');
-                ?>
-                    <script>
-                        function Deconnexion() {
-                            alert("Déconnexion réussite !");
-                            }
-                    </script>
-                <?php
-                } else {
-                    echo ('<a class="dropdown-item" href="Login.php">Se connecter</a>');
-                    echo ('<a class="dropdown-item" href="Inscription.php">S\'inscrire</a>');
-                }
-                ?>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+<html lang="fr">
+    <?php require_once $_SESSION["APPLICATION"].'/FONCTIONNALITE/header.php'; ?>
+    <body>
+    <?php require_once $_SESSION["APPLICATION"].'/FONCTIONNALITE/nav.php'; ?>
 <!--------------------------------------------------------------------------------------------------------------------------------------------->   
         <div class="jumbotron">
-          <div class="container">
-              <h1>Cette page n'existe plus...</h1>
-              <h4>Vite, retournez à l'accueil... :o)</h4>
-              <ul>
-                  <li><a href="accueil.html.php">Accueil</a></li>
-                  <li><a href="besoin.html.php">Besoins</a></li>
-                  <li><a href="talent.html.php">Talents</a></li>
-                  <li><a href="categorie.html.php">Catégories</a></li>
-              </ul>
-          </div>
-        </div>
-        <?php 
-            require_once 'footer.php'; 
-        ?>
-  </body>
+            <div class="container">
+                <h1>Cette page n'existe plus...</h1>
+                <h4>Vite, retournez à l'accueil... :o)</h4>
+            </div>
+        </div>       
+        <?php require_once $_SESSION["APPLICATION"].'/FONCTIONNALITE/footer.php'; ?>       
+    </body>
 </html>
